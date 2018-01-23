9899c63528ad26e0654e022e492f1aaf266f8204
Hony <hony.com@seznam.cz>
2018-01-23 19:49:03 +0100

    ----

--- levels/123456.php
+++ levels/123456.php
@@ -16,12 +16,9 @@
        <h1>LEVEL 2</h1>
   </div>
   <div>
-           <a href="http://purkiada.sspbrno.cz/men-in-black/uloha2/imgs/jpg.JPG">
-		<img src="http://purkiada.sspbrno.cz/men-in-black/uloha2/imgs/jpg.JPG" height="400" alt="purkyňka"></a>
-
+		<img src="http://purkiada.sspbrno.cz/men-in-black/uloha2/imgs/jpg.JPG" height="400" alt="purkyňka">
     <form action="./index.php" method="GET">
       <input type="text" class="elements form-control" name="pass">
-
       <input type="submit" class="btn btn-lg elements" value="Zkusím štěstí">
     </form>
   <br>
